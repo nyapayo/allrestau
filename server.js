06270cdd36eb0f0bd027d11bafae9c06772b825b
siaka <siakajunior1997@gmail.com>
2020-02-28 11:27:04 +0100

    add cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const express = require('express');
 
 const app = express(), port = process.env.PORT || 4000, host = 'localhost';
 
-app.listen(port, host, err => {
+app.get('/', (req, res) => {
+	res.send('Nodejs realtime app');
+}).get('/data', (req, res) => {
+	res.json([{name: 'siaka', age: 24, jeans: 'dior'}]);
+}).listen(port, host, err => {
 	if (!err) {
 		console.log(`Server is running on http://${host}:${port}`);
 	}
