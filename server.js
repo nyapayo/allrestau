6cbaee47cfe1fe7eadeb9393bd0222912613a47b
siaka <38745915+nyapayo@users.noreply.github.com>
2025-02-18 16:52:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,59 +1,22 @@
-const http = require('http');
+"use strict";
+
+const cors = require('cors');
 const express = require('express');
+const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server);
-const PORT = process.env.PORT || 4000, host = 'localhost';
-const router = require('./router');
 
-// install helmet middleware
+const { PORT = 5000 } = process.env;
 
-const devMode = true;
+app.use(cors());
+app.use(express.json());
 
-app.use(router);
-// Gestion des reponses 404
-app.use((req, res, next) => {
-	res.set('content-type', 'text/html');
-	res.status(404).send('<h3>Sorry can not find that</h3>'+'<p style="color: red">'+req.originalUrl+'</p>');
+app.get('/', (req, res) => {
+  res.send('Nodejs express reactjs');
 });
 
-// Gestion des erreurs
-// logError
-app.use((err, req, res, next) => {
-	console.error(err.stack); next(err);
+server.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server ready at http://localhost:${PORT}`)
+  }
 });
-// clientErrorHandler 
-app.use((err, req, res, next) => {
-	if (req.xhr) {
-		res.status(500).json({ error: 'Something failed!' });
-	} else {
-		next(err);
-	}
-});
-// error handler
-app.use((err, req, res, next) => {
-	res.status(500).json({error: err});
-});
-
-if (!devMode) {
-	// point to restau app after run 'npm run build'
-	// express will serve our static files
-	app.use(express.static(__dirname+'restau/build'));
-}
-
-// Listen for socket connesxion
-io.on('connection', socket => {
-	console.log('New user connected');
-
-	socket.on('disconnect', () => {
-		console.log('User disconnected!');
-	});
-
-});
-
-server.listen(PORT, err => {
-	if (!err) {
-		console.log(`Server is running on http://${host}:${PORT}`);
-	}
-});
-
